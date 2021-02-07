37549b2c3bf88130e430cde3bdd276c3c7009415
Michael Pingel <mdpingel@gmail.com>
2021-02-07 10:29:31 -0800

    end of lesson

--- src/main.js
+++ src/main.js
@@ -2,30 +2,28 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Triangle from './js/triangle.js';
-import Rectangle from './js/rectangle.js';
 
 $(document).ready(function() {
-  $('#triangle-checker-form').submit(function(event) {
-    event.preventDefault();
-    const length1 = parseInt($('#length1').val());
-    console.log(`side1 length is ${length1}`);
-    const length2 = parseInt($('#length2').val());
-    console.log(`side2 length is ${length2}`);
-    const length3 = parseInt($('#length3').val());
-    console.log(`side3 length is ${length3}`);
-    const triangle = new Triangle(length1, length2, length3);
-    console.log(triangle);
-    const response = triangle.checkType();
-    $('#response').append("<p>" + response + "</p>");
-  });
-  
-  $('#rectangle-area-form').submit(function(event) {
-    event.preventDefault();
-    const length1 = parseInt($('#rect-length1').val());
-    const length2 = parseInt($('#rect-length2').val());
-    const rectangle = new Rectangle(length1, length2);
-    const response = rectangle.getArea();
-    $('#response2').append(`<p>The area of the rectangle is ${response}.</p>`);
+  $('#weatherLocation').click(function() {
+    const city = $('#location').val();
+    $('#location').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvin is ${response.main.temp} degrees.`);
+    }
   });
 });
